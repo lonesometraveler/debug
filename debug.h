d7fa0525e0aa9a174b96f446e811a2e681cfc569
lonesometraveler <lonesometraveler@mac.com>
2020-03-17 09:07:52 -0400

    use debug instead of printf

--- debug.h
+++ debug.h
@@ -5,13 +5,13 @@
 
 #ifdef DEBUG 
 
-#define DBG(...) printf("" __VA_ARGS__)
+#define DBG(...) debug("" __VA_ARGS__)
 
 #define DBGREF(fmt, ...)                            \
     do {                                            \
-        printf("[DBG] %s %s, line:%d\r\n",          \
+        debug("[DBG] %s %s, line:%d\r\n",          \
                __func__, __FILE__, __LINE__);       \
-        printf(fmt, ##__VA_ARGS__);                 \
+        debug(fmt, ##__VA_ARGS__);                 \
     } while (0)
 
 #else 
